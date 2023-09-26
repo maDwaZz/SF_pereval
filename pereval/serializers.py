33a371679f709875b808c1408c4b864a2c71f99a
maDwaZz <madwazz1@gmail.com>
2023-09-26 19:09:00 +0300

    Figuring out serializers

--- pereval/serializers.py
+++ pereval/serializers.py
@@ -3,19 +3,20 @@ from rest_framework import serializers
 from pereval.models import *
 
 
-class PerevalSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PerevalAdded
-        fields = '__all__'
+class PerevalSerializer(serializers.Serializer):
+    beauty_title = serializers.CharField()
+    title = serializers.CharField()
+    other_titles = serializers.CharField()
+    connect = serializers.CharField()
+    add_time = serializers.DateTimeField()
+    email = serializers.EmailField(source=user.email)
+    fam = serializers.CharField(source=user.last_name)
+    name = serializers.CharField(source=user.first_name)
+    latitude = serializers.FloatField()
+    longtitude = serializers.FloatField()
+    height = serializers.IntegerField()
+    level_winter = serializers.CharField()
+    level_summer = serializers.CharField()
+    level_autumn = serializers.CharField()
+    level_spring = serializers.CharField()
 
-
-class CoordsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Coords
-        fields = '__all__'
-
-
-class PerevalImagesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PerevalImages
-        fields = '__all__'
