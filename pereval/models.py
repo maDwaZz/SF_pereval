a3f3ddcb7b582d3174d822533349e4cae5167315
maDwaZz <madwazz1@gmail.com>
2023-10-02 20:14:30 +0300

    Start of work with new methods

--- pereval/models.py
+++ pereval/models.py
@@ -3,13 +3,14 @@ from django.db import models
 
 
 class PerevalUser(models.Model):
-    username = models.CharField(max_length=50, verbose_name='Имя пользователя')
     first_name = models.CharField(max_length=50, verbose_name='Имя')
     last_name = models.CharField(max_length=50, verbose_name='Фамилия')
-    email = models.EmailField(blank=False, verbose_name='Электронная почта')
+    sur_name = models.CharField(max_length=50, verbose_name='Отчество', blank=True)
+    email = models.EmailField(blank=False, unique=True, verbose_name='Электронная почта')
+    phone = models.CharField(max_length=50, verbose_name='Телефон', blank=True)
 
     def __str__(self):
-        return self.username
+        return self.email
 
     class Meta:
         verbose_name = 'Пользователь'
